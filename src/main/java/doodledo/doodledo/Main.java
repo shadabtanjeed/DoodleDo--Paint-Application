c73d50f2e05a127cf759b853a2173655b5d73316
shadabtanjeed <stanjeed@gmail.com>
2024-04-12 23:18:10 +0600

    renamed the fxml file

--- src/main/java/doodledo/doodledo/Main.java
+++ src/main/java/doodledo/doodledo/Main.java
@@ -16,7 +16,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("main_window.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("DoodleDo");
         stage.setMaximized(true);
